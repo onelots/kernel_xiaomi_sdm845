3f9f8e2ff45edc977aa7e2145d1b5dde0e77b202
Onelots <onelots@lineageos.org>
2026-07-14 21:30:29 +0200

    mm/vmalloc.c: define calc_total_vmalloc_size as __maybe_unused
    Signed-off-by: Onelots <onelots@lineageos.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -393,7 +393,7 @@ int is_vmalloc_addr(const void *x)
 	return 1;
 }
 
-static void calc_total_vmalloc_size(void)
+static void __maybe_unused calc_total_vmalloc_size(void)
 {
 	total_vmalloc_size = VMALLOC_END - POSSIBLE_VMALLOC_START -
 		vmalloc_reserved;
@@ -406,7 +406,7 @@ int is_vmalloc_addr(const void *x)
 	return addr >= VMALLOC_START && addr < VMALLOC_END;
 }
 
-static void calc_total_vmalloc_size(void) { }
+static void __maybe_unused calc_total_vmalloc_size(void) { }
 #endif
 EXPORT_SYMBOL(is_vmalloc_addr);
 
